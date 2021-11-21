3417ffca81e17ff95e2007591e5d4d265b27aa74
Prasad <prasad.chaudhari@interlinksoftware.com>
2021-11-21 16:34:00 +0000

    Spring Boot main class for TransferService and its test

---
+++ src/test/java/com/example/transferservice/TransferServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.transferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferServiceApplicationTest {
+
+  @Test
+  void contextLoads() {
+    TransferServiceApplication.main(new String[] {});
+  }
+}
