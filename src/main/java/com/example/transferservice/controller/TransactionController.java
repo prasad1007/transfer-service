fbb0aea688876ab18f5e4a7ad7910d6f7c87ec50
Prasad <prasad.chaudhari@interlinksoftware.com>
2021-11-21 16:34:58 +0000

    Controller class for endpoint /transfer and its integration test

---
+++ src/main/java/com/example/transferservice/controller/TransactionController.java
@@ -0,0 +1,47 @@
+package com.example.transferservice.controller;
+
+import com.example.transferservice.exception.AccountNotExistException;
+import com.example.transferservice.exception.NoSufficientBalanceException;
+import com.example.transferservice.model.rest.TransferRequest;
+import com.example.transferservice.model.rest.TransferResponse;
+import com.example.transferservice.service.AccountsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Objects;
+
+import static com.example.transferservice.constant.UriConstants.TRANSACTION_API;
+import static com.example.transferservice.constant.UriConstants.TRANSFER_URI;
+
+@RestController
+@RequestMapping(TRANSACTION_API)
+public class TransactionController {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TransactionController.class);
+
+  @Autowired private final AccountsService accountService;
+
+  private TransactionController(final AccountsService accountService) {
+    this.accountService = Objects.requireNonNull(accountService);
+  }
+
+  @PostMapping(TRANSFER_URI)
+  public ResponseEntity<TransferResponse> transferMoney(
+      @RequestBody @Valid final TransferRequest transferRequest)
+      throws AccountNotExistException, NoSufficientBalanceException {
+    /*TODO Prasad - We know before logging statement is logging actual account numbers. In actual implementation, this should NOT be
+     *  practiced and even if we want to log them, we should encrypt them for e.g. 34*****1 */
+    LOGGER.info(
+        "Transferring amount from  account {} to account {}",
+        transferRequest.getSourceAccountNumber(),
+        transferRequest.getDestinationAccountNumber());
+    return ResponseEntity.ok(accountService.transferBalances(transferRequest));
+  }
+}
---
+++ src/test/java/com/example/transferservice/controller/TransactionControllerIT.java
@@ -0,0 +1,70 @@
+package com.example.transferservice.controller;
+
+import static com.example.transferservice.constant.UriConstants.TRANSACTION_API;
+import static com.example.transferservice.constant.UriConstants.TRANSFER_URI;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.example.transferservice.TransferServiceApplication;
+import com.example.transferservice.model.rest.TransferRequest;
+import com.example.transferservice.model.rest.TransferResponse;
+
+@SpringBootTest(classes = TransferServiceApplication.class, webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+class TransactionControllerIT {
+
+  private static final long SOURCE_ACCOUNT_NUMBER = 34000001L;
+  private static final long DESTINATION_ACCOUNT_NUMBER = 34000002L;
+
+  @LocalServerPort private int port;
+
+  final TestRestTemplate restTemplate = new TestRestTemplate();
+
+  @Test
+  void testIfTransferBalanceFunctionalityIsWorkingAsExpected() {
+    final TransferRequest transferRequest =
+        new TransferRequest(SOURCE_ACCOUNT_NUMBER, DESTINATION_ACCOUNT_NUMBER, new BigDecimal(100));
+    final ResponseEntity<TransferResponse> responseEntity =
+        restTemplate.postForEntity(
+            createURLWithPort(TRANSACTION_API + TRANSFER_URI), transferRequest, TransferResponse.class);
+    assertThat(responseEntity.getStatusCode(), is(OK));
+  }
+
+  @Test
+  void testIfTransferBalanceFunctionalityIsWorkingAsExpectedWhenAccountNotExistExceptionHappens() {
+    final TransferRequest transferRequest =
+        new TransferRequest(34000003L, DESTINATION_ACCOUNT_NUMBER, new BigDecimal(100));
+    final ResponseEntity<TransferResponse> responseEntity =
+        restTemplate.postForEntity(
+            createURLWithPort(TRANSACTION_API + TRANSFER_URI), transferRequest, TransferResponse.class);
+    assertThat(responseEntity.getStatusCode(), is(NOT_FOUND));
+  }
+
+  @Test
+  void
+      testIfTransferBalanceFunctionalityIsWorkingAsExpectedWhenNoSufficientBalanceExceptionHappens() {
+    final TransferRequest transferRequest =
+        new TransferRequest(SOURCE_ACCOUNT_NUMBER, DESTINATION_ACCOUNT_NUMBER, new BigDecimal(500));
+    final ResponseEntity<TransferResponse> responseEntity =
+        restTemplate.postForEntity(
+            createURLWithPort(TRANSACTION_API + TRANSFER_URI), transferRequest, TransferResponse.class);
+    assertThat(responseEntity.getStatusCode(), is(BAD_REQUEST));
+  }
+
+  private String createURLWithPort(final String uri) {
+    return "http://localhost:" + port + "/" + uri;
+  }
+}
