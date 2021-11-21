cb4ed7089333b08b94ced89f408df1ccb8a69533
Prasad <prasad.chaudhari@interlinksoftware.com>
2021-11-21 16:35:55 +0000

    Constant classes added to hold global constants and URI constants

---
+++ src/main/java/com/example/transferservice/constant/GlobalConstants.java
@@ -0,0 +1,6 @@
+package com.example.transferservice.constant;
+
+public final class GlobalConstants {
+
+  public static final String CONSTANTS_CLASS_DONT_CONSTRUCT = "Constants class don't construct";
+}
---
+++ src/main/java/com/example/transferservice/constant/UriConstants.java
@@ -0,0 +1,12 @@
+package com.example.transferservice.constant;
+
+public final class UriConstants {
+
+  private UriConstants() {
+    throw new UnsupportedOperationException(GlobalConstants.CONSTANTS_CLASS_DONT_CONSTRUCT);
+  }
+
+  public static final String TRANSACTION_API = "v1/transaction";
+
+  public static final String TRANSFER_URI = "/transfer";
+}
