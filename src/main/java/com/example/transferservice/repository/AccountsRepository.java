ff89b0ff465c35311c9322d11ba91278c70dcb16
Prasad <prasad.chaudhari@interlinksoftware.com>
2021-11-21 16:38:43 +0000

    Repository class added to perform operations on table ACCOUNTS

---
+++ src/main/java/com/example/transferservice/repository/AccountsRepository.java
@@ -0,0 +1,23 @@
+package com.example.transferservice.repository;
+
+import java.util.Optional;
+
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.transferservice.entity.Account;
+
+@Transactional(readOnly = true)
+public interface AccountsRepository extends JpaRepository<Account, Long> {
+
+  Optional<Account> findByAccountNumber(final Long accountNumber);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Transactional
+  @Query("SELECT a FROM Account a WHERE a.accountNumber = ?1")
+  Optional<Account> getAccountForUpdate(final Long accountNumber);
+}
